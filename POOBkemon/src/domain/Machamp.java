5fa559c82c287c4b6716bf1e46e50960ff13f837
CatizPlum <125584788+CatizPlum@users.noreply.github.com>
2025-05-17 19:44:41 -0500

    Update Machamp.java
    documentacion

--- POOBkemon/src/domain/Machamp.java
+++ POOBkemon/src/domain/Machamp.java
@@ -2,7 +2,22 @@ package domain;
 
 import java.util.ArrayList;
 
+/**
+ * Clase que representa al Pokémon Machamp, un Pokémon de tipo Lucha puro.
+ * Conocido por su increíble fuerza física y sus cuatro brazos musculados.
+ * Especializado en ataques físicos brutales y combate cuerpo a cuerpo.
+ */
 public class Machamp extends AbstractPokemon {
+
+    /**
+     * Constructor de Machamp. Inicializa sus estadísticas base, tipo y movimientos.
+     * Características principales:
+     * - Ataque físico extremadamente alto (394)
+     * - Defensa física sólida (284)
+     * - HP considerable (384)
+     * - Velocidad baja (207)
+     * - Tipo Lucha puro
+     */
     public Machamp() {
         this.name = "Machamp";
         this.primaryType = Type.FIGHTING;
@@ -16,10 +31,13 @@ public class Machamp extends AbstractPokemon {
         this.speed = 207;
 
         this.moves = new ArrayList<>();
-
         initializeMoves();
     }
 
+    /**
+     * Inicializa los movimientos que Machamp puede aprender por defecto.
+     * Incluye movimientos físicos y técnicas básicas de combate.
+     */
     @Override
     protected void initializeMoves() {
         learnMove("Leer");
@@ -30,15 +48,23 @@ public class Machamp extends AbstractPokemon {
         learnMove("Water Pulse");
     }
 
-    // Habilidad pasiva: aumenta daño físico
+    /**
+     * Habilidad pasiva: Brute Force Boost.
+     * Aumenta el poder de todos los movimientos físicos en un 25%.
+     *
+     * @param move El movimiento a potenciar
+     *
+     * Efecto:
+     * - Incrementa el poder base de movimientos físicos
+     * - Muestra mensaje informando del aumento de poder
+     * - No afecta movimientos especiales o de estado
+     */
     public void applyPassiveAbility(Move move) {
         if (move.getCategory() == MoveCategory.PHYSICAL) {
             double boostedPower = move.getPower() * 1.25;
             move.setPower((int) boostedPower);
-            System.out.println(this.name + "'s BruteForceBoost increases " + move.getName() + "'s power to " + move.getPower());
+            System.out.println(this.name + "'s BruteForceBoost increases " +
+                    move.getName() + "'s power to " + move.getPower());
         }
     }
-
-
-
 }
